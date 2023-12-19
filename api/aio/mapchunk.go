2c9ce577731b37aa0113f67022b970b4ab78031e
Oswaldoooo! <oswaldohome007@gmail.com>
2023-12-19 17:24:12 +0800

    Update mapchunk.go

--- api/aio/mapchunk.go
+++ api/aio/mapchunk.go
@@ -1,98 +1,99 @@
 package aio
 import (
-    "encoding/json"
-    "errors"
-    "fmt"
-    "os"
-    "syscall"
-    "unsafe"
+        "errors"
+        "fmt"
+        "os"
+        "syscall"
+        "unsafe"
+        "io"
 )
 
 type Chunk struct{
-    fd int
-    b []byte
-    realsize int64
+        fd int
+        fdcloser io.Closer
+        b []byte
+        realsize int64
 }
-func mapchunk(path string,chunksize int64)(fd int,b []byte,err error){
-    //var fd int
-    fd,err=syscall.Open(path,syscall.O_CREAT|syscall.O_RDWR,0640);
-    if err==nil&&fd>0{
-    //  defer syscall.Close(fd)
+func mapchunk(fd int,chunksize int64)(b []byte,err error){
         err=syscall.Ftruncate(fd,chunksize)
         if err==nil{
-            b,err=syscall.Mmap(fd,0,int(chunksize),syscall.PROT_READ|syscall.PROT_WRITE,syscall.MAP_SHARED)
+                b,err=syscall.Mmap(fd,0,int(chunksize),syscall.PROT_READ|syscall.PROT_WRITE,syscall.MAP_SHARED)
         }
-        if err!=nil{
-            syscall.Close(fd)
-            fd=0
-        }
-    }
-    if fd<=0{
-        err=errors.New("open "+path+" failed")
-    }
-    return
+        return
 }
 func(s *Chunk)unmap()error{
-    syscall.Syscall(syscall.SYS_MSYNC,uintptr(unsafe.Pointer(&s.b[0])),syscall.MS_SYNC,0)
-    err:=syscall.Munmap(s.b)
-    if err==nil{
-        s.realsize=0
-        s.b=nil
-    }
-    return err
+        syscall.Syscall(syscall.SYS_MSYNC,uintptr(unsafe.Pointer(&s.b[0])),syscall.MS_SYNC,0)
+        err:=syscall.Munmap(s.b)
+        if err==nil{
+                s.realsize=0
+                s.b=nil
+        }
+        return err
 }
 func (s *Chunk)Close()error{
-    var err error
-    err=syscall.Close(s.fd)
-    if err==nil{
+        var err error
+        if s.fdcloser==nil{
+                err=syscall.Close(s.fd)
+                if err==nil{
 
-    }
-    err=s.unmap()
-    return err
+                }
+        }else{
+                err=s.fdcloser.Close()
+        }
+        err=s.unmap()
+        return err
 }
 //write to chunk;off_t is start
 func (s *Chunk)Marshal(fun func(any)([]byte,error),v any,off_t uint)(length int,err error){
-    var b []byte
-    b,err=fun(v)
-    if err==nil{
-        if int(off_t)+len(b)>len(s.b){
-            err=errors.New("out of range")
-            return
+        var b []byte
+        b,err=fun(v)
+        if err==nil{
+                if int(off_t)+len(b)>len(s.b){
+                        err=errors.New("out of range")
+                        return
+                }
+                copy(s.b[off_t:int(off_t)+len(b)],b)
+                length=len(b)
         }
-        copy(s.b[off_t:int(off_t)+len(b)],b)
-        length=len(b)
-    }
-    return
+        return
 }
 func OpenChunk(filename string,size int64)*Chunk{
-    var ans =new(Chunk)
-    var err error
-    ans.fd,ans.b,err=mapchunk(filename,size)
-    if err==nil{
-        return ans
-    }else{
-        fmt.Fprintln(os.Stderr,err.Error())
-    }
-    return nil
+        var ans =new(Chunk)
+        //var err error
+        //ans.fd,ans.b,err=mapchunk(filename,size)
+        f,err:=Open(filename,syscall.O_CREAT|syscall.O_RDWR)
+        if err==nil{
+                ans.b,err=mapchunk(f.Fd(),size)
+                if err==nil{
+                        ans.fd=f.Fd()
+                        ans.fdcloser=f
+                        return ans
+                }
+                f.Close()
+        }
+        if err!=nil{
+                fmt.Fprintln(os.Stderr,err.Error())
+        }
+        return nil
 }
 func CStrLen(p []byte)int{
-    var ans int=0
-    for _,e:=range p{
-        if e!=0{
-            ans++
-        }else{
-            break
+        var ans int=0
+        for _,e:=range p{
+                if e!=0{
+                        ans++
+                }else{
+                        break
+                }
         }
-    }
-  return ans
-  }
+        return ans
+}
 func (s *Chunk)ReMap(newsize int64)error{
-    err:=s.unmap()
-    if err==nil{
-        err=syscall.Ftruncate(s.fd,newsize)
+        err:=s.unmap()
         if err==nil{
-            s.b,err=syscall.Mmap(s.fd,0,int(newsize),syscall.PROT_READ|syscall.PROT_WRITE,syscall.MAP_SHARED)
+                err=syscall.Ftruncate(s.fd,newsize)
+                if err==nil{
+                        s.b,err=syscall.Mmap(s.fd,0,int(newsize),syscall.PROT_READ|syscall.PROT_WRITE,syscall.MAP_SHARED)
+                }
         }
-    }
-    return err
+        return err
 }
