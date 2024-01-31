6ddf01795200fdc790a60e38ea4f9dec8914b272
Oswaldoooo! <oswaldohome007@gmail.com>
2024-01-31 21:25:22 +0800

    add pointer

---
+++ sys/pointer.go
@@ -0,0 +1,30 @@
+package sys
+
+import "unsafe"
+
+type Pointer[T any] struct {
+	T *T
+	b []byte
+}
+
+func NewPointer[T any]() *Pointer[T] {
+	var (
+		ans = &Pointer[T]{}
+		t   T
+	)
+	ans.b = make([]byte, unsafe.Sizeof(t))
+	ans.T = (*T)(unsafe.Pointer(&ans.b[0]))
+	return ans
+}
+func NewPointer2[T any](b []byte) *Pointer[T] {
+	var (
+		t T
+	)
+	if len(b) >= int(unsafe.Sizeof(t)) {
+		return &Pointer[T]{b: b, T: (*T)(unsafe.Pointer(&b[0]))}
+	}
+	return nil
+}
+func (s *Pointer[T]) Out() []byte {
+	return s.b
+}
